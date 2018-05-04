6e503162d9e137867fd34823891beeaa57f4a9d5
Fritz Lin <uxfritz@163.com>
2018-05-04 20:50:47 +0800

    chore: enhance npmignore 'n pack test

--- examples/main.js
+++ examples/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 
+// import VueThreejs from '@/../package/lib/' // testing pack
+// import VueThreejs from '@/../lib/' // testing build
 import VueThreejs from '@'
 Vue.use(VueThreejs)
 
