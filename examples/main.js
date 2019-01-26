8fa3057d3460c7bf812b153de8f06cd05c3c8e75
Fritz Lin <uxfritz@163.com>
2019-01-27 01:26:13 +0800

    wip: added --name to lib build and sync'd `import VueThreejs from ..` in ./examples/main.js

--- examples/main.js
+++ examples/main.js
@@ -3,8 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 
-// import VueThreejs from '@/../package/lib/' // testing pack
-// import VueThreejs from '@/../lib/' // testing build
+// import VueThreejs from '@/../package/lib/VueThreejs.common' // testing pack
+// import VueThreejs from '@/../lib/VueThreejs.common' // testing build
 import VueThreejs from '@'
 Vue.use(VueThreejs)
 
